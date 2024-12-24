a7877c521a897cf8cb3748802268396167e06f15
danone05 <106051099+danone05@users.noreply.github.com>
2024-12-24 21:36:45 +0500

    force.js
    force

--- force/force.js
+++ force/force.js
@@ -1,78 +1,44 @@
-const readline = require('readline');
-
-function findOccurrencesBruteForce(S, T) { // Brute Force
-    let n = S.length;
-    let m = T.length;
-    let positions = [];
-
-    for (let i = 0; i <= n - m; i++) {
-        let match = true;
-        for (let j = 0; j < m; j++) {
-            if (S[i + j] !== T[j]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) {
-            positions.push(i + 1); // ���������� � 1
+function bruteForce(text, textToFind) {
+    arrOfSubstrings = new Array();
+    r = textToFind.length; //длина подстроки, она по сути является длиной смещения
+    l = text.length; //длина изначальной строки. чтобы мы знали, до куда идти в цикле.
+    cnt = 0; //по сути нужно чтобы просто добавлять новое вхождения в наш эррей
+    for (let i = 0; i < l - r + 1; i++) {
+        if (text.substring(i, i + r) == textToFind) {
+            arrOfSubstrings[cnt] = i;
+            cnt++;
         }
     }
-
-    return positions;
+    return arrOfSubstrings;
 }
 
-function findOccurrencesWithHash(S, T) {    // ����
-    let n = S.length;
-    let m = T.length;
-
-    let targetHash = 0;
-    for (let i = 0; i < m; i++) {
-        targetHash += T.charCodeAt(i);  // ����� ����� �������� � ������ T
+function ascii_hashes(text, textToFind) {
+    hashOfSubstring = 0;
+    hashOfCurrentText = 0;
+    r = textToFind.length; //длина подстроки, она по сути является длиной смещения
+    l = text.length; //длина изначальной строки. чтобы мы знали, до куда идти в цикле.
+    cnt = 0; //по сути нужно чтобы просто добавлять новое вхождения в наш эррей
+    arrOfSubstrings = new Array();
+    collisions = 0;
+    for (let i = 0; i < r; i++) {
+        hashOfSubstring += textToFind[i].charCodeAt(0);
+        hashOfCurrentText += text[i].charCodeAt(0);
     }
-
-    let currentHash = 0;
-    for (let i = 0; i < m; i++) {
-        currentHash += S.charCodeAt(i);  // ����� ����� �������� ��� ������� ���������
-    }
-
-    let positions = [];
-
-    for (let i = 0; i <= n - m; i++) {
-        if (currentHash === targetHash) {
-            let match = true;
-            for (let j = 0; j < m; j++) {
-                if (S[i + j] !== T[j]) {
-                    match = false;
-                    break;
-                }
+    for (let i = 0; i <= l - r; i++) {
+        if (hashOfCurrentText === hashOfSubstring) {
+            if (text.substring(i, i + r) == textToFind) {
+                arrOfSubstrings[cnt] = i;
+                cnt++;
             }
-
-            if (match) {
-                positions.push(i + 1);  // ������� � �������� � 1
+            else {
+                collisions += 1;
             }
         }
-
-        if (i < n - m) {
-            currentHash -= S.charCodeAt(i);
-            currentHash += S.charCodeAt(i + m);
-        }
+        if (i != l - r)
+            hashOfCurrentText = hashOfCurrentText - text[i].charCodeAt(0) + text[i + r].charCodeAt(0);
     }
-
-    return positions;
+    console.log("количество коллизий: " + collisions.toString());
+    return arrOfSubstrings;
 }
 
-// ������ �������������
-let S1 = "abcabdecab";
-let T1 = "cab";
-console.log(findOccurrencesBruteForce(S1, T1));  // �����: [3, 8]
-console.log(findOccurrencesWithHash(S1, T1));  // �����: [3, 8]
-
-let S2 = "sads2asdsad";
-let T2 = "sad";
-console.log(findOccurrencesBruteForce(S2, T2));  // �����: [1, 3, 5, 9]
-console.log(findOccurrencesWithHash(S2, T2));  // �����: [1, 3, 5, 9]
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+console.log(ascii_hashes('abrakadbabra', 'ab'));
